8ba34ce3840cacf430875c73901976dbf4a980ad
ravi-2101 <ravirajmobh.vision@gmail.com>
2025-03-11 18:12:47 +0530

    unwanted remove from app-modules file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserController } from './user/user.controller';
 import { UserModule } from './user/user.module';
 
 @Module({
